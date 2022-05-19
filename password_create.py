d6a8af9c8cb18aa5f310a47f18a0291b4e9a4544
ddreddick1029 <reddickd@appstate.edu>
2022-05-19 12:30:34 -0400

    Added user input loop

--- password_create.py
+++ password_create.py
@@ -1,27 +1,41 @@
 import random
 import string
+import sys
 
-print("Welcome to the Password Generator!")
+another = True
+
+while another:
+    print("Welcome to the Password Generator!")
 
 # ask user for password length requirement
-length = int(input("How many characters does your password require?: "))
+    length = int(input("How many characters does your password require?: "))
 
 # initialize character types
 
-lower = string.ascii_lowercase
-capitalize = string.ascii_uppercase
-nums = string.digits
-symbols = string.punctuation
+    lower = string.ascii_lowercase
+    capitalize = string.ascii_uppercase
+    nums = string.digits
+    symbols = string.punctuation
 
 # combine all char types
-combine = lower + capitalize + nums + symbols
+    combine = lower + capitalize + nums + symbols
 
 # randomize character types and add length requirement
-randomization = random.sample(combine, length)
+    randomization = random.sample(combine, length)
 
 # construct the secure password
-password = "".join(randomization)
+    password = "".join(randomization)
 
 # display password
-print(password)
+    print(password)
 
+    another = input("Would you like to generate a different secure password?: ")
+    while True:
+        if another == 'yes':
+            generate = True
+            break
+        elif another == 'no':
+            generate = False
+            sys.exit()
+        else:
+            another = input('Invalid response! Enter "yes" to continue or "no" to exit: ')
